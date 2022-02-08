80a0b744e26317bace40416c9fa75b0ff02debf1
hayden brown <haydenbrowncinci@gmail.com>
2022-02-08 08:37:03 -0500

    Add project files.

---
+++ DoudExFeb7/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoudExFeb7 {
+    public class Account {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+
+        public bool Deposit(decimal Amount) {
+            if(Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            Balance += Amount;
+            return true;
+        }
+        public bool Withdrawl(decimal Amount) {
+            if(Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            if(Amount > Balance) {
+                throw new Exception("Insuficient Funds!");
+            }
+            Balance -= Amount;
+            return true;
+        }
+        
+    }
+}
---
+++ DoudExFeb7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DoudExFeb7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acct1 = new Account();
+            acct1.Deposit(1000);
+            Console.WriteLine($"{Account}|")
+        }
+    }
+}
